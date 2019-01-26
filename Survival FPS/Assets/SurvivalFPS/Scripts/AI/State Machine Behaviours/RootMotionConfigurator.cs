e280038f6877be1a0ea17f953c710c0aa81c2c1e
MSI\dw218192 <tongweid@usc.edu>
2019-01-25 16:18:35 -0800

    reload bug fix

--- Survival FPS/Assets/SurvivalFPS/Scripts/AI/State Machine Behaviours/RootMotionConfigurator.cs
+++ Survival FPS/Assets/SurvivalFPS/Scripts/AI/State Machine Behaviours/RootMotionConfigurator.cs
@@ -9,17 +9,20 @@ namespace SurvivalFPS.AI
         [SerializeField] private int m_RootPosition = 0;
         [SerializeField] private int m_RootRotation = 0;
 
+        private bool m_Incremented = false;
+
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             if (m_StateMachine)
             {
                 m_StateMachine.AddRootMotionRequest(m_RootPosition, m_RootRotation);
+                m_Incremented = true;
             }
         }
 
         override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            if (m_StateMachine)
+            if (m_StateMachine && m_Incremented)
             {
                 m_StateMachine.AddRootMotionRequest(-m_RootPosition, -m_RootRotation);
             }
